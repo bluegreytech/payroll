887bf4c829ebcb2eaf40c6fa02c930586decaf17
Binny29 <52149842+Binny29@users.noreply.github.com>
2019-09-03 12:36:11 +0530

    add

---
+++ logs/log-2019-09-03.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-09-03 08:09:08 --> 404 Page Not Found: /index
+ERROR - 2019-09-03 08:09:08 --> Severity: Warning --> include(application/\errors\html\error_404.php): failed to open stream: No such file or directory C:\xampp\htdocs\payroll\system\core\Exceptions.php 182
+ERROR - 2019-09-03 08:09:08 --> Severity: Warning --> include(): Failed opening 'application/\errors\html\error_404.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\payroll\system\core\Exceptions.php 182
