885642acae3e16e4751d6a520fe8aae75b63c82b
Binny29 <52149842+Binny29@users.noreply.github.com>
2019-09-03 11:30:10 +0530

    add

---
+++ emailtemplate.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+   <html>
+    <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Welcome to {site_name}</title>
+       
+    </head>
+    <body>
+         <table cellspacing="0" style="border: 2px; width: 500px; height: 70px;">
+            <tr>
+                <td style="background: #eee; border-bottom: 1px solid #f88120;"><img src="http://nyalkaran.bluegreytech.co.in/admin/default/images/logo/logo.png" alt="Nyalkaran Group Logo" style="width: 121px;"></td>
+              
+            </tr>
+           
+        </table>
+
+      
+        <table cellspacing="0" style="border: 2px; width: 500px; height: 400px;background: #eee;">
+            <tr>
+                <td>Hello {username},</td>
+            </tr>
+            <tr >
+               <td><p>We were told that you forgot your password on {username}.</p>
+                
+                <p>To reeset your password,please click this link: <a>{reset_link}</a></p>
+                
+               </td>
+            </tr>
+            <tr>
+               <td><p>Regard,</p>
+               <p>Nyalkaran Group,</p></td>
+            </tr>
+        </table>
+        <table cellspacing="0" style="border: 2px; width: 500px; height: 50px;">
+            <tr>
+                <td style="background: #283f82;"><p style="color: #fff; text-align: center;">Copyright © {year} Nyalkaran Group. All Rights Reserved</p></td>
+              
+            </tr>
+           
+        </table>
+        
+    </body>
+</html>
+
+
