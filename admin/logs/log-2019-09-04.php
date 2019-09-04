00435d3f6ab7aa861c4a2630bc61d31708990c52
Binny29 <52149842+Binny29@users.noreply.github.com>
2019-09-04 15:51:01 +0530

    add

--- admin/logs/log-2019-09-04.php
+++ admin/logs/log-2019-09-04.php
@@ -2,3 +2,11 @@
 
 ERROR - 2019-09-04 12:10:02 --> 404 Page Not Found: Assets/img
 ERROR - 2019-09-04 12:10:17 --> 404 Page Not Found: Assets/img
+ERROR - 2019-09-04 12:10:42 --> 404 Page Not Found: Assets/img
+ERROR - 2019-09-04 12:10:50 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\payroll\admin\application\controllers\Login.php 24
+ERROR - 2019-09-04 12:10:51 --> 404 Page Not Found: Assets/img
+ERROR - 2019-09-04 12:10:51 --> 404 Page Not Found: Assets/img
+ERROR - 2019-09-04 12:10:51 --> 404 Page Not Found: Assets/img
+ERROR - 2019-09-04 12:10:51 --> 404 Page Not Found: Assets/img
+ERROR - 2019-09-04 12:10:51 --> 404 Page Not Found: Default/assets
+ERROR - 2019-09-04 12:13:23 --> 404 Page Not Found: Default/cdn-cgi
