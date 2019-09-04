4621bbf2df243adcf5a277623c0a595565d3e0a6
Binny29 <52149842+Binny29@users.noreply.github.com>
2019-09-04 15:40:27 +0530

    add

---
+++ admin/logs/log-2019-09-04.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-09-04 12:10:02 --> 404 Page Not Found: Assets/img
+ERROR - 2019-09-04 12:10:17 --> 404 Page Not Found: Assets/img
