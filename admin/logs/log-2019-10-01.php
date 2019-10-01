df4a9ef19497d8fa9c39a3eb3ecff4f11a856a72
2300 <himanibhatt17@gmail.com>
2019-10-01 02:37:24 -0400

    add

---
+++ admin/logs/log-2019-10-01.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-10-01 07:40:10 --> 404 Page Not Found: Assets/img
+ERROR - 2019-10-01 07:43:53 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\payroll\admin\application\controllers\Login.php 47
+ERROR - 2019-10-01 07:43:53 --> Severity: Notice --> Undefined variable: session C:\xampp\htdocs\payroll\admin\application\controllers\Login.php 105
+ERROR - 2019-10-01 07:44:24 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\payroll\admin\application\controllers\Login.php 47
+ERROR - 2019-10-01 07:44:24 --> Severity: Notice --> Undefined variable: session C:\xampp\htdocs\payroll\admin\application\controllers\Login.php 105
+ERROR - 2019-10-01 07:44:32 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\payroll\admin\application\controllers\Login.php 47
+ERROR - 2019-10-01 07:44:32 --> Severity: Notice --> Undefined variable: session C:\xampp\htdocs\payroll\admin\application\controllers\Login.php 105
+ERROR - 2019-10-01 07:45:33 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\payroll\admin\application\controllers\Login.php 47
+ERROR - 2019-10-01 07:45:33 --> 404 Page Not Found: Assets/img
+ERROR - 2019-10-01 07:45:33 --> 404 Page Not Found: Assets/img
+ERROR - 2019-10-01 07:45:33 --> 404 Page Not Found: Assets/img
+ERROR - 2019-10-01 07:45:33 --> 404 Page Not Found: Assets/img
+ERROR - 2019-10-01 07:50:16 --> Severity: Notice --> Undefined variable: paymentopt C:\xampp\htdocs\payroll\admin\application\views\Invoice\add-invoice.php 90
+ERROR - 2019-10-01 07:50:16 --> Severity: Notice --> Undefined variable: paymentopt C:\xampp\htdocs\payroll\admin\application\views\Invoice\add-invoice.php 105
+ERROR - 2019-10-01 07:50:17 --> Severity: Notice --> Undefined variable: amount C:\xampp\htdocs\payroll\admin\application\views\Invoice\add-invoice.php 169
+ERROR - 2019-10-01 07:50:17 --> Severity: Notice --> Undefined variable: addtax C:\xampp\htdocs\payroll\admin\application\views\Invoice\add-invoice.php 183
+ERROR - 2019-10-01 07:50:17 --> Severity: error --> Exception: Too few arguments to function Invoice::getedithr(), 0 passed in C:\xampp\htdocs\payroll\admin\system\core\CodeIgniter.php on line 532 and exactly 1 expected C:\xampp\htdocs\payroll\admin\application\controllers\Invoice.php 105
---
+++ hr/logs/log-2019-10-01.php
@@ -0,0 +1,119 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-10-01 07:41:18 --> 404 Page Not Found: Assets/img
+ERROR - 2019-10-01 08:27:28 --> 404 Page Not Found: Home/assets
+ERROR - 2019-10-01 08:27:28 --> 404 Page Not Found: Home/assets
+ERROR - 2019-10-01 08:27:28 --> 404 Page Not Found: Home/assets
+ERROR - 2019-10-01 08:27:28 --> 404 Page Not Found: Home/assets
+ERROR - 2019-10-01 08:27:29 --> 404 Page Not Found: Default/assets
+ERROR - 2019-10-01 08:27:32 --> Query error: Unknown column 'Is_deleted' in 'where clause' - Invalid query: SELECT *
+FROM `tblhr`
+WHERE `Is_deleted` = '0'
+AND `hr_type` != '1'
+ORDER BY `hr_id` DESC
+ERROR - 2019-10-01 08:27:50 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:27:50 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:27:50 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:27:50 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:27:50 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:27:50 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:27:50 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:27:50 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:27:50 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:27:50 --> 404 Page Not Found: Uploads/default
+ERROR - 2019-10-01 08:27:56 --> Query error: Unknown column 'Is_deleted' in 'where clause' - Invalid query: SELECT *
+FROM `tblhr`
+WHERE `Is_deleted` = '0'
+AND `hr_type` != '1'
+ORDER BY `hr_id` DESC
+ERROR - 2019-10-01 08:28:03 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:03 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:03 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:03 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:03 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:03 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:03 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:03 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:03 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:06 --> Query error: Unknown column 'company_id' in 'where clause' - Invalid query: SELECT *
+FROM `tblcmpholiday`
+WHERE `Is_deleted` = '0'
+AND `company_id` = '2'
+ORDER BY `holiday_id` DESC
+ERROR - 2019-10-01 08:28:08 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:08 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:08 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:08 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:08 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:08 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:08 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:08 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:08 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:13 --> 404 Page Not Found: Adminmaster/adminlist
+ERROR - 2019-10-01 08:28:15 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:15 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:15 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:15 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:15 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:15 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:15 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:15 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:15 --> Severity: Notice --> Undefined property: stdClass::$companylogo C:\xampp\htdocs\payroll\hr\application\views\Company\companylist.php 91
+ERROR - 2019-10-01 08:28:31 --> Query error: Unknown column 'company_id' in 'where clause' - Invalid query: SELECT *
+FROM `tblcmpholiday`
+WHERE `Is_deleted` = '0'
+AND `company_id` = '2'
+ORDER BY `holiday_id` DESC
+ERROR - 2019-10-01 08:29:06 --> Query error: Unknown column 'Is_deleted' in 'where clause' - Invalid query: SELECT *
+FROM `tblhr`
+WHERE `Is_deleted` = '0'
+AND `hr_type` != '1'
+ORDER BY `hr_id` DESC
+ERROR - 2019-10-01 08:32:59 --> Query error: Unknown column 'Is_deleted' in 'where clause' - Invalid query: SELECT *
+FROM `tblhr`
+WHERE `Is_deleted` = '0'
+AND `hr_type` != '1'
+ORDER BY `hr_id` DESC
+ERROR - 2019-10-01 08:33:04 --> Query error: Unknown column 'company_id' in 'where clause' - Invalid query: SELECT *
+FROM `tblcmpholiday`
+WHERE `Is_deleted` = '0'
+AND `company_id` = '2'
+ORDER BY `holiday_id` DESC
+ERROR - 2019-10-01 08:33:21 --> 404 Page Not Found: Adminmaster/adminlist
+ERROR - 2019-10-01 08:33:30 --> Query error: Unknown column 'Is_deleted' in 'where clause' - Invalid query: SELECT *
+FROM `tblhr`
+WHERE `Is_deleted` = '0'
+AND `hr_type` != '1'
+ORDER BY `hr_id` DESC
+ERROR - 2019-10-01 08:33:35 --> Query error: Unknown column 'company_id' in 'where clause' - Invalid query: SELECT *
+FROM `tblcmpholiday`
+WHERE `Is_deleted` = '0'
+AND `company_id` = '2'
+ORDER BY `holiday_id` DESC
+ERROR - 2019-10-01 08:33:41 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:33:41 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:33:41 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:33:42 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:33:42 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:33:42 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:33:42 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:33:42 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:33:42 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:53 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:53 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:53 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:53 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:53 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:53 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:54 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:54 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:54 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:56 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:56 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:56 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:56 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:56 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:56 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:56 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:56 --> 404 Page Not Found: Attendance/assets
+ERROR - 2019-10-01 08:35:56 --> 404 Page Not Found: Attendance/assets
