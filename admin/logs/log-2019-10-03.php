dc8dff2600495833b4a9c9a51213fb45e9d91f82
2300 <himanibhatt17@gmail.com>
2019-10-03 03:39:55 -0400

    add

---
+++ admin/logs/log-2019-10-03.php
@@ -0,0 +1,5 @@
+ERROR - 2019-10-03 09:34:39 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\payroll\admin\application\controllers\Login.php 70
+ERROR - 2019-10-03 09:34:39 --> 404 Page Not Found: Assets/img
+ERROR - 2019-10-03 09:34:40 --> 404 Page Not Found: Assets/img
+ERROR - 2019-10-03 09:34:40 --> 404 Page Not Found: Assets/img
+ERROR - 2019-10-03 09:34:40 --> 404 Page Not Found: Assets/img
