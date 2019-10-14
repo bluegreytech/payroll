80f40eb76cd3f6840bb13bdaa3ae284bb56889c8
bluegreytech <52150104+bluegreytech@users.noreply.github.com>
2019-10-14 08:22:13 -0400

    add

--- admin/application/views/Employee/employelist - Copy.php
+++ admin/application/views/Employee/employelist - Copy.php
@@ -1,7 +1,6 @@
 ﻿<?php 
 	 $this->load->view('common/header.php');
 	 $this->load->view('common/sidebar.php');
-
 ?>
 
 
@@ -25,35 +24,6 @@
                 <div class="content container-fluid">
 
 
-				<?php if(($this->session->flashdata('error'))){ ?>
-
-				<div class="alert alert-danger" id="errorMessage">
-
-				<strong> <?php echo $this->session->flashdata('error'); ?></strong> 
-
-				</div>
-
-				<?php } ?>
-
-				<?php if(($this->session->flashdata('success'))){ ?>
-
-						<div class="alert alert-success" id="successMessage">
-
-						<strong> <?php echo $this->session->flashdata('success'); ?></strong> 
-
-						</div>
-
-				<?php } ?>
-
-				<?php if(($this->session->flashdata('warning'))){ ?>
-
-				<div class="alert alert-warning" id="warningMessage">
-
-				<strong> <?php echo $this->session->flashdata('warning'); ?></strong> 
-
-				</div>
-
-				<?php } ?>
 
 				
 
@@ -65,27 +35,13 @@
 
 					<div class="row">
 
-							<div class="col-sm-5 col-5">
+						<div class="col">
 
-							<h4 class="page-title">List of Company   </h4>
+							<h4 class="page-title">List of Employee  </h4>
 
-							</div>
+						</div>	
 
-							<div class="col-sm-7 col-7 text-right m-b-30">
-
-							<a href="<?php echo base_url();?>Company/companyadd" class="btn add-btn"><i class="fa fa-plus">
-
-
-
-</i> Add Company</a>
-
-							</div>
-
-						</div>
-
-
-
-					
+					</div>
 
 
 
@@ -93,15 +49,81 @@
 
 
 
+					<?php if(($this->session->flashdata('error'))){ ?>
 
 
-						<!-- Search Filter -->
 
-						<form method="post" action="<?php echo base_url();?>Company">
+							<div class="alert alert-danger" id="errorMessage">
+
+
+
+							<strong> <?php echo $this->session->flashdata('error'); ?></strong> 
+
+
+
+							</div>
+
+
+
+							<?php } ?>
+
+
+
+							<?php if(($this->session->flashdata('success'))){ ?>
+
+
+
+									<div class="alert alert-success" id="successMessage">
+
+
+
+									<strong> <?php echo $this->session->flashdata('success'); ?></strong> 
+
+
+
+									</div>
+
+
+
+							<?php } ?>
+
+
+
+							<?php if(($this->session->flashdata('warning'))){ ?>
+
+
+
+							<div class="alert alert-warning" id="warningMessage">
+
+
+
+							<strong> <?php echo $this->session->flashdata('warning'); ?></strong> 
+
+
+
+							</div>
+
+
+
+							<?php } ?>
+
+
+
+					
+
+
+
+					<!-- Search Filter -->
+
+
+
+					<form method="post" action="<?php echo base_url();?>Employee">
+
+
 
 						<div class="row filter-row">
 
-						
+
 
 							<div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-12">  
 
@@ -111,25 +133,27 @@
 
 											<option value=""> -- Select -- </option>
 
-											<option value="companytype">Company Type</option>
-
 											<option value="companyname">Company Name</option>
 
-											<option value="comemailaddress">Email Address</option>
+											<option value="first_name">Employee Name</option>
 
-											<option value="comcontactnumber">Contact Number</option>
+											<option value="email">Email Address</option>
 
-											<option value="emailverifystatus">Verification Status</option>
+											<option value="phone">Contact Number</option>
+
+											<option value="department">Department</option>
+
+											<option value="desgination">Designation</option>
 
 										</select>
-
-										<!-- <label class="focus-label">Role</label> -->
 
 									</div>
 
 							</div>
 
-							<div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-12" >  
+
+
+						<div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-12" >  
 
 									<div class="form-group form-focus box" id='business'>
 
@@ -179,57 +203,88 @@
 
 								<input type="submit" value="Search" class="btn btn-success btn-block">
 
-							</div> 
+							</div>    
 
 							<div class="col-md-3"> 
 
-								<a href="<?php echo base_url()?>Company" class="btn btn-info"><i class="fa fa-refresh"></i></a> 
+								<a href="<?php echo base_url()?>Employee" class="btn btn-info"><i class="fa fa-refresh"></i></a> 
 
 							</div> 
 
-							    
-
 						</div> 
+
+
 
 					</form>
 
+
+
 					
+
+
 
 					<!-- Search Filter -->
 
 
 
+					
+
+
+
 					<div class="row">
+
+
 
 						<div class="col-md-12">
 
+
+
 							<div class="table-responsive">
 
-								
+
+
+				
 								 <!-- <table class="table table-striped custom-table datatable" class="display" style="width:100%"> -->
 								 <table id="example" class="display table table-striped custom-table" style="width:100%">
 
+
 									<thead>
+
+
 
 										<tr>
 
+
+
 											<th>No</th>
 
-											<th>Company Name</th>
+											<th>Name</th>
 
 											<th>Contact Number</th>
 
-											<th>Company Type</th>
+											<th>Email Address</th>
 
-											<th>Email Address </th>
+											<th>From Company</th>
 
-											<th>Verification Status </th>
+											<th>Department</th>
 
-											<th class="text-right">Action</th>
+											<th>Designation</th>
+
+											<!-- <th class="text-right">Action</th> -->
+
+
+
+											
+
+
 
 										</tr>
 
+
+
 									</thead>
+
+
 
 									<tbody>
 
@@ -237,13 +292,15 @@
 
 										$i=1;
 
-										if($companyData){                             
+										if($employeeData){                             
 
-										foreach($companyData as $comp)
+										foreach($employeeData as $empData)
 
 										{
 
 									?>
+
+
 
 										<tr>
 
@@ -251,19 +308,19 @@
 
 										<td>
 
-											<h2 class="table-avatar">
+											<span  class="table-avatar">
 
 											<?php 
 
-											if($comp->companyimage!='')
+											if($empData->ProfileImage!='')
 
 											{
 
 												?>
 
-												<a href="<?php echo base_url();?>Company/profile/<?php echo $comp->companyid;?>" title="show company profile" class="avatar"><img src="<?php echo base_url();?>upload/company/<?php echo $comp->companyimage;?>"></a>
+												<a href="<?php echo base_url();?>Employee/profile/<?php echo $empData->emp_id;?>" title="show employee profile" class="avatar"><img src="http://payroll.bluegreytech.co.in/hr/upload/emp/<?php echo $empData->ProfileImage;?>"></a>
 
-												<a href="<?php echo base_url();?>Company/profile/<?php echo $comp->companyid;?>" title="show company profile"><?php echo $comp->companyname ;?> </a>
+												<a href="<?php echo base_url();?>Employee/profile/<?php echo $empData->emp_id;?>" title="show employee profile"><?php echo $empData->first_name;?></a>
 
 												<?php
 
@@ -277,9 +334,9 @@
 
 												?>
 
-												<a href="<?php echo base_url();?>Company/profile/<?php echo $comp->companyid;?>" title="show company profile" class="avatar"><img src="<?php echo base_url();?>upload/default/avtar.jpg" ></a>
+												<a href="<?php echo base_url();?>Employee/profile/<?php echo $empData->emp_id;?>" title="show employee profile" class="avatar"><img src="<?php echo base_url();?>upload/default/avtar.jpg"></a>
 
-												<a href="<?php echo base_url();?>Company/profile/<?php echo $comp->companyid;?>" title="show company profile"><?php echo $comp->companyname ;?> </a>
+												<a href="<?php echo base_url();?>Employee/profile/<?php echo $empData->emp_id;?>" title="show employee profile"><?php echo $empData->first_name;?></a>
 
 												<?php
 
@@ -291,79 +348,35 @@
 
 											</td>
 
+										<td><?php echo $empData->phone ;?></td>
 
+										<td><?php echo $empData->email ;?></td>
 
-										
+										<td><?php echo $empData->companyname ;?></td>
 
-										<td><?php echo $comp->comcontactnumber ;?></td>
+										<td><?php echo $empData->department ;?></td>
 
-										<td><?php echo $comp->companytype ;?></td>
-
-										<td><?php echo $comp->comemailaddress ;?></td>
-
-										
-
-										<td>	
-
-												<div class="action-label">
-
-												<a class="btn btn-white btn-sm btn-rounded">
-
-												<?php if($comp->emailverifystatus=='Verify')
-
-												{?>
-
-													<i class="fa fa-dot-circle-o 
-
-												<?php if($comp->emailverifystatus=='Verify'){ echo "text-success";}?>"></i>Verify
-
-												<?php
-
-												}
-
-												else
-
-												{
-
-													?>
-
-													<i class="fa fa-dot-circle-o 
-
-												<?php if($comp->emailverifystatus==null){ echo "text-danger";}?>"></i>Pending
-
-												<?php
+										<td><?php echo $empData->desgination ;?></td>
 
 
 
-												}
+											<td class="text-right">
 
-												?>
+													<!-- <a class="dropdown-item" href="<?php //echo base_url();?><?php //echo $empData->emp_id;?>" role="button">Show Profile </a> -->
 
-												</a>
-
-												</div>
+														<!-- <a class="dropdown-item" onclick="deletedata(<?php// echo $empData->emp_id; ?>)" data-toggle="modal" data-target="#delete_employee"><i class="fa fa-trash-o m-r-5"></i> Delete</a> -->
 
 											</td>
 
 
 
-										<td class="text-center">
+											
 
-											<a href="<?php echo base_url();?>Company/editcompany/<?php echo $comp->companyid;?>" role="button" title="Edit">
 
-													<i class="fa fa-pencil m-r-5"></i> </a>
 
-											<a  onclick="deletedata(<?php echo $comp->companyid; ?>)" data-toggle="modal" data-target="#delete_client" title="Delete"><i class="fa fa-trash-o m-r-5"></i> </a>
+										
 
-										    <a  href="<?php echo base_url();?>Company/company_notification_expired/<?php echo $comp->companyid;?>" role="button"  title="Notification">
 
-											<i class="fa fa-bell-o" aria-hidden="true"></i></a>
-
-											<a  href="<?php echo base_url();?>Leave/leavelist" role="button" title="Show Leaves Types">
-
-													<i class="fa fa-calendar-check-o" aria-hidden="true"></i></a>				
-
-										</td>
 
 
 
@@ -449,7 +462,7 @@
 
 
 
-				<div class="modal custom-modal fade" id="delete_client" role="dialog">
+				<div class="modal custom-modal fade" id="delete_employee" role="dialog">
 
 
 
@@ -469,7 +482,7 @@
 
 
 
-									<h3>Delete Company</h3>
+									<h3>Delete Employee</h3>
 
 
 
@@ -557,9 +570,7 @@
 
 
 
-			
-
-
+	
 
         </div>
 
@@ -569,20 +580,13 @@
 
 
 
-		
-
-
-
 		<!-- Sidebar Overlay -->
-
-
 
 		<div class="sidebar-overlay" data-reff=""></div>
 
-        <?php $this->load->view('common/footer');?>
+		<?php $this->load->view('common/footer');?>
 
-		
-<script>
+		<script>
 	$(document).ready(function() {
 	 $('#example').DataTable( {
 		aaSorting: [[0, 'asc']],
@@ -617,8 +621,8 @@
 	 {
 		extend: 'pdfHtml5',
 		text:'<i class="fa fa-file-pdf-o"></i> PDF',
-		title: "List of Company",
-		filename:"List_of_Company",
+		title: "List of Hr",
+		filename:"List_of_Hr",
 		orientation: 'landscape', 
 		pageSize: 'A4',		
 		exportOptions: {
@@ -662,11 +666,7 @@
 
 </script>
 
-
-
-	
-
-
+		
 
     </body>
 
@@ -715,8 +715,6 @@ $(document).ready(function(){
     });
 
 });
-
-
 
 
 
@@ -792,18 +790,44 @@ $(function() {
 
 
 
-function deletedata(companyid){  
-			$('#delete_client').modal('show')
+function deletedata(emp_id){  
+
+			$('#delete_employee').modal('show')
+
 				$('#yes_btn').click(function(){
+
 						Url="<?php echo base_url();?>"
+
 						$.ajax({
-						url: Url+'/Company/delete_company/',
+
+						url: Url+'/Employee/delete_employee/',
+
 						type: "post",
-						data: {companyid:companyid} ,
+
+						data: {emp_id:emp_id} ,
+
 						success: function (response) {             
+
 					// document.location.href = url+'adminmaster/adminlist/';          
+
 					},
+
+
+
 					})
+
+
+
 				});
+
+
+
 		}			
+
+
+
+
+
+
+
 </script>
--- admin/logs/log-2019-10-14.php
+++ admin/logs/log-2019-10-14.php
@@ -262,3 +262,24 @@ ERROR - 2019-10-14 12:55:51 --> 404 Page Not Found: Adminmaster/default
 ERROR - 2019-10-14 12:56:38 --> 404 Page Not Found: Adminmaster/default
 ERROR - 2019-10-14 12:57:00 --> 404 Page Not Found: Adminmaster/default
 ERROR - 2019-10-14 12:57:39 --> 404 Page Not Found: Adminmaster/default
+ERROR - 2019-10-14 13:05:18 --> 404 Page Not Found: Adminmaster/default
+ERROR - 2019-10-14 13:05:21 --> 404 Page Not Found: Adminmaster/default
+ERROR - 2019-10-14 13:06:06 --> 404 Page Not Found: Adminmaster/default
+ERROR - 2019-10-14 13:07:10 --> 404 Page Not Found: Adminmaster/default
+ERROR - 2019-10-14 13:07:45 --> 404 Page Not Found: Adminmaster/default
+ERROR - 2019-10-14 13:07:49 --> 404 Page Not Found: Company/default
+ERROR - 2019-10-14 13:13:12 --> 404 Page Not Found: Invoice/default
+ERROR - 2019-10-14 13:18:59 --> 404 Page Not Found: Invoice/default
+ERROR - 2019-10-14 13:21:44 --> 404 Page Not Found: Invoice/default
+ERROR - 2019-10-14 13:22:12 --> 404 Page Not Found: Invoice/default
+ERROR - 2019-10-14 13:22:20 --> 404 Page Not Found: Invoice/default
+ERROR - 2019-10-14 13:23:11 --> 404 Page Not Found: Company/default
+ERROR - 2019-10-14 13:23:15 --> 404 Page Not Found: Company/default
+ERROR - 2019-10-14 13:23:28 --> 404 Page Not Found: Company/default
+ERROR - 2019-10-14 13:23:47 --> 404 Page Not Found: Company/default
+ERROR - 2019-10-14 13:23:55 --> 404 Page Not Found: Invoice/default
+ERROR - 2019-10-14 13:57:34 --> Severity: Notice --> Undefined variable: hrData C:\xampp\htdocs\payroll\admin\application\views\Employee\employelist.php 271
+ERROR - 2019-10-14 14:11:45 --> 404 Page Not Found: Upload/no_image
+ERROR - 2019-10-14 14:11:45 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'GROUP BY U.first_name ORDER BY  att.attendance_id DESC' at line 1 - Invalid query: SELECT att.attendance_id as attendanceid,CONCAT(U.first_name, ' ', U.last_name) as firstlast,U.ProfileImage as ProfileImage,SUM(IF(DATE(att.attendance_date) ='2019-09-01',att.attendance_id,0)) AS 'abc1',SUM(IF(DATE(att.attendance_date) ='2019-09-02',att.attendance_id,0)) AS 'abc2',SUM(IF(DATE(att.attendance_date) ='2019-09-03',att.attendance_id,0)) AS 'abc3',SUM(IF(DATE(att.attendance_date) ='2019-09-04',att.attendance_id,0)) AS 'abc4',SUM(IF(DATE(att.attendance_date) ='2019-09-05',att.attendance_id,0)) AS 'abc5',SUM(IF(DATE(att.attendance_date) ='2019-09-06',att.attendance_id,0)) AS 'abc6',SUM(IF(DATE(att.attendance_date) ='2019-09-07',att.attendance_id,0)) AS 'abc7',SUM(IF(DATE(att.attendance_date) ='2019-09-08',att.attendance_id,0)) AS 'abc8',SUM(IF(DATE(att.attendance_date) ='2019-09-09',att.attendance_id,0)) AS 'abc9',SUM(IF(DATE(att.attendance_date) ='2019-09-10',att.attendance_id,0)) AS 'abc10',SUM(IF(DATE(att.attendance_date) ='2019-09-11',att.attendance_id,0)) AS 'abc11',SUM(IF(DATE(att.attendance_date) ='2019-09-12',att.attendance_id,0)) AS 'abc12',SUM(IF(DATE(att.attendance_date) ='2019-09-13',att.attendance_id,0)) AS 'abc13',SUM(IF(DATE(att.attendance_date) ='2019-09-14',att.attendance_id,0)) AS 'abc14',SUM(IF(DATE(att.attendance_date) ='2019-09-15',att.attendance_id,0)) AS 'abc15',SUM(IF(DATE(att.attendance_date) ='2019-09-16',att.attendance_id,0)) AS 'abc16',SUM(IF(DATE(att.attendance_date) ='2019-09-17',att.attendance_id,0)) AS 'abc17',SUM(IF(DATE(att.attendance_date) ='2019-09-18',att.attendance_id,0)) AS 'abc18',SUM(IF(DATE(att.attendance_date) ='2019-09-19',att.attendance_id,0)) AS 'abc19',SUM(IF(DATE(att.attendance_date) ='2019-09-20',att.attendance_id,0)) AS 'abc20',SUM(IF(DATE(att.attendance_date) ='2019-09-21',att.attendance_id,0)) AS 'abc21',SUM(IF(DATE(att.attendance_date) ='2019-09-22',att.attendance_id,0)) AS 'abc22',SUM(IF(DATE(att.attendance_date) ='2019-09-23',att.attendance_id,0)) AS 'abc23',SUM(IF(DATE(att.attendance_date) ='2019-09-24',att.attendance_id,0)) AS 'abc24',SUM(IF(DATE(att.attendance_date) ='2019-09-25',att.attendance_id,0)) AS 'abc25',SUM(IF(DATE(att.attendance_date) ='2019-09-26',att.attendance_id,0)) AS 'abc26',SUM(IF(DATE(att.attendance_date) ='2019-09-27',att.attendance_id,0)) AS 'abc27',SUM(IF(DATE(att.attendance_date) ='2019-09-28',att.attendance_id,0)) AS 'abc28',SUM(IF(DATE(att.attendance_date) ='2019-09-29',att.attendance_id,0)) AS 'abc29',SUM(IF(DATE(att.attendance_date) ='2019-09-30',att.attendance_id,0)) AS 'abc30'FROM tblattendance att LEFT JOIN tblemp U ON U.emp_id = att.emp_id   WHERE YEAR(attendance_date) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)AND MONTH(attendance_date) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH) and companyid= default GROUP BY U.first_name ORDER BY  att.attendance_id DESC
+ERROR - 2019-10-14 14:11:45 --> 404 Page Not Found: Upload/no_image
+ERROR - 2019-10-14 14:19:20 --> 404 Page Not Found: Invoice/default
