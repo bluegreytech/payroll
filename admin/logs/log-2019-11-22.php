7ca5f6deb7d0ba4671ec247b732c7fcfd3bb2b0b
himani_vyas <himanibhatt17@gmail.com>
2019-11-22 01:34:48 -0500

    add

---
+++ admin/logs/log-2019-11-22.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-11-22 07:31:51 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\payroll\admin\application\controllers\Login.php 48
+ERROR - 2019-11-22 07:31:51 --> Query error: Unknown column 'Is_deleted' in 'where clause' - Invalid query: SELECT *
+FROM `tblhr`
+WHERE `Is_deleted` = '0'
