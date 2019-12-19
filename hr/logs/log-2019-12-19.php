ab305b64d43f79aa8ad950c389333c2042dd361a
himani_vyas <himanibhatt17@gmail.com>
2019-12-19 11:02:36 +0530

    add

---
+++ hr/logs/log-2019-12-19.php
@@ -0,0 +1,5 @@
+ERROR - 2019-12-19 06:25:13 --> 404 Page Not Found: Default/js
+ERROR - 2019-12-19 06:25:13 --> 404 Page Not Found: Default/js
+ERROR - 2019-12-19 06:25:14 --> 404 Page Not Found: Default/css
+ERROR - 2019-12-19 06:25:14 --> 404 Page Not Found: Default/css
+ERROR - 2019-12-19 06:25:17 --> 404 Page Not Found: Assets/img
