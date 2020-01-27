b06ceeb783f3243d1f10e12dbc1cb51c97a02c80
veeru225 <52230630+veeru225@users.noreply.github.com>
2020-01-27 13:20:34 +0530

    add

--- admin/logs/log-2020-01-27.php
+++ admin/logs/log-2020-01-27.php
@@ -1,3 +1,9 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 ERROR - 2020-01-27 06:28:45 --> Query error: Table 'payrolldb.tblactivitylog' doesn't exist - Invalid query: INSERT INTO `tblactivitylog` (`AdminId`, `Module`, `Activity`) VALUES ('1', 'Login', 'Admin login with record id: 1')
+ERROR - 2020-01-27 07:53:53 --> 404 Page Not Found: Default/css
+ERROR - 2020-01-27 07:53:53 --> 404 Page Not Found: Default/js
+ERROR - 2020-01-27 07:53:53 --> 404 Page Not Found: Default/js
+ERROR - 2020-01-27 07:53:53 --> 404 Page Not Found: Default/plugins
+ERROR - 2020-01-27 07:54:05 --> 404 Page Not Found: Default/img
+ERROR - 2020-01-27 07:54:36 --> 404 Page Not Found: Default/img
---
+++ hr/logs/log-2020-01-27.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-01-27 07:53:36 --> 404 Page Not Found: Assets/img
